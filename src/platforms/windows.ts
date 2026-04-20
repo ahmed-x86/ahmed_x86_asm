d20ae3b91e8414662ad05f7be168687d23084191
ahmed-x86 <ahmdansmr7@gmail.com>
2026-04-20 17:17:12 +0200

    .

--- src/platforms/windows.ts
+++ src/platforms/windows.ts
@@ -105,7 +105,7 @@ export async function detectBestWin32Linker(context: vscode.ExtensionContext): P
     });
 }
 
-// 3. دالة التعرف التلقائي الخاصة بالويندوز
+
 export function detectWindowsOption(fileText: string): { index: number, name: string } {
     const textLower = fileText.toLowerCase();
     
